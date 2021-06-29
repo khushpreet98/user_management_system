72565f537885b774bd37108acbce28c9a554851d
khushidhalio <khushpreet@wanclouds.net>
2021-06-29 09:39:22 +0530

    :updated

--- app/users/model.py
+++ app/users/model.py
@@ -2,12 +2,9 @@ from sqlalchemy import Integer, Column, String, DateTime
 from app import db
 from datetime import datetime
 
-
-
 class User(db.Model):
 
     __tablename__ = "users"
-
     id = Column(Integer(), primary_key=True)
     first_name = Column(String(100))
     last_name = Column(String(100))
@@ -15,6 +12,4 @@ class User(db.Model):
     salary = Column(Integer())
     address = Column(String(100))
     created_at = Column(DateTime, default = datetime.utcnow())
-
-
 
--- app/users/service.py
+++ app/users/service.py
@@ -1,22 +1,17 @@
 from app import db
 from typing import List
 from .model import User
-from .interface import user_interface
+from .interface import UserInterface
 from sqlalchemy_filters import apply_filters
-import json
 
-
-class user_service:
-
-
+class UserService:
     @staticmethod
-    def create(new_attrs: user_interface) -> User:
+    def create(new_attrs: UserInterface) -> User:
         new_user = User(first_name=new_attrs["first_name"], last_name=new_attrs["last_name"],full_name=new_attrs["full_name"],
         salary=new_attrs["salary"], address=new_attrs["address"])
         db.session.add(new_user)
         db.session.commit()
         return new_user
-
 
     @staticmethod
     def get_all(filters) -> List[User]:
@@ -27,22 +22,15 @@ class user_service:
         return f_query.all()
 
     @staticmethod
-    def get_by_name(first_name:str) -> List[User]:
-        return User.query.filter(User.first_name == first_name).all()
-
-
-    @staticmethod
     def get_by_id(id: int) -> User:
         return User.query.get(id)
 
-
     @staticmethod
     def delete_by_id(id: int) -> List[int]:
-
         users = User.query.filter(User.id == id).first()
         if not users:
             return []
         db.session.delete(users)
         db.session.commit()
         return [id]
-
+    
